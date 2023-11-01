c3548278beec60cbc0e1a4b162b5c4a73f80a6be
ldgen <leondmikol@mail.ru>
2023-11-01 15:03:54 +0500

    коммит (картинки и постраничный вывод)

--- Akhmetova_Glazki/Akhmetova_Glazki/AkhmetovaModel.Context.cs
+++ Akhmetova_Glazki/Akhmetova_Glazki/AkhmetovaModel.Context.cs
@@ -22,7 +22,6 @@ namespace Akhmetova_Glazki
                 _context = new Akhmetova_glazkiEntities();
             return _context;
         }
-
         public Akhmetova_glazkiEntities()
             : base("name=Akhmetova_glazkiEntities")
         {
